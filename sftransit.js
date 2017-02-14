7ec50204c1db93423719fa1082e5bf3f7fae4482
kmesic <kenmesi@yahoo.com>
2017-02-14 02:53:01 -0800

    Added tooltips to neighborhoods and modularized code a bit.

--- sftransit.js
+++ sftransit.js
@@ -8,47 +8,79 @@ var scaleRatio = 270000/1587;
 
 var center = [-122.433701, 37.767683];
 
-// Create svg for the map
+var neighborhoodsStyle = {
+    "fill": "#f93",
+    "stroke": "black",
+    "stroke-width": "1"
+}
+
 var zoom = d3.behavior.zoom()
     .translate([0, 0])
     .scale(1)
     .scaleExtent([1, 8])
     .on("zoom", zoomMap);
 
+// Create svg for the map
 var svg  = d3.select("body")
              .append("svg")
              .attr("id", "sf-map")
              .attr("viewBox", "0 0 " + width + " " + height )
              .attr("preserveAspectRatio", "xMinYMin");
 
-var g = svg.append("g");
+// Create tooltip for neighborhoods
+var tooltip = d3.select('body')
+                .append('div')
+                .attr('class', 'hide tooltip');
 
+var geoPath, projection;
+
+var mapData = [];
 
 function zoomMap() {
-    g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    for (var i = 0; i < mapData.length; i++)  {
+        mapData[i].attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    }
 }
 
-d3.json("sfmaps/neighborhoods.json", createMap);
-
-function createMap(data) {
-    var projection = d3.geo.mercator()
+createMap();
+function createMap() {
+    projection = d3.geo.mercator()
         .scale(width * scaleRatio)
         .center(center)
         .translate([width /2, height /1.9]);
 
-    var geoPath = d3.geo.path()
+    geoPath = d3.geo.path()
         .projection(projection);
 
-
-    g.selectAll("path")
-        .data(data.features)
-        .enter()
-        .append( "path" )
-        .attr( "d", geoPath )
-        .style( "fill", "#f93" )
-        .style("stroke", "black")
-        .style("stroke-width", 1);
+    d3.json("sfmaps/neighborhoods.json", function(data) {
+        var map = addData(data.features, "neighborhood");
+        map.on('mousemove', function(currData) {
+                var mouse = d3.mouse(svg.node()).map(function(loc) {
+                    return parseInt(loc);
+                });
+                tooltip.classed('hide', false)
+                    .attr('style', 'left:' + (mouse[0] + 15) +
+                      'px; top:' + (mouse[1] - 35) + 'px')
+                    .html(currData.properties.neighborho);
+            })
+            .on('mouseout', function() {
+                    tooltip.classed('hide', true);
+            });
+    });
 
     svg.call(zoom) 
        .call(zoom.event);
+}
+
+
+function addData(data, dataName) {
+    var g = svg.append("g");
+    mapData.push(g);
+
+    return g.selectAll("path")
+            .data(data)
+            .enter()
+            .append( "path" )
+            .attr( "d", geoPath )
+            .attr("class", dataName);
 }
